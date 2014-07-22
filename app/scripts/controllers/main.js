0c1593d59d74b208a42c9c4e8fab37f97e59570d
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-07-21 20:47:54 -0400

    setting up angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rebeccamurphygithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rebeccamurphygithubioApp
+ */
+angular.module('rebeccamurphygithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
