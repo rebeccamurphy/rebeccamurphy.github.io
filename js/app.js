b8fa7484ff100a0df72d4d5b58eed4e6edbbbd2b
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2015-09-20 22:09:37 -0400

    got tabs working woo.

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(function(){
+	var app = angular.module('store',[]);
+
+	app.controller('StoreController', function(){
+		this.products = gems;
+	});	
+	app.controller('PanelController', function(){
+		this.tab =1;
+		this.setTab=function(setTab){
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab){
+			return this.tab===checkTab;
+		};
+	});
+	var gems=[
+		{
+			name: "Dodecahedron",
+			price:2.95,
+			description: '...',
+			canPurchase:true,
+			soldOut:true,
+		},
+		{
+			name: "Dodecahedron",
+			price: 5.95,
+			description: '...',
+			canPurchase:false,
+			soldOut:true,
+		}
+	];
+})();
