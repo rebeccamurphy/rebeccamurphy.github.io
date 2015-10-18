91d40c9d5b6abbf4d6263866ceb164fb4c513fc7
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2015-10-17 23:05:48 -0400

    started one page angular design and sass

---
+++ js/script.js
@@ -0,0 +1,40 @@
+(function(){
+	var app = angular.module('website',['ngRoute']);
+
+	app.controller('mainController', function($scope){
+		$scope.message ="main page";
+	
+	});	
+	app.controller('aboutController', function($scope) {
+        $scope.message = 'Look! I am an about page.';
+    });
+
+    app.controller('contactController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+	// configure our routes
+    app.config(function($routeProvider) {
+        $routeProvider
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+            .when('/home', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
+    });
+
+})();
